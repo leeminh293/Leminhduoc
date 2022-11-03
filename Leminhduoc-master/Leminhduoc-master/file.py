a777d023b90fcf541b8973c09d5f6acb2e451685
leeminh293 <65277610+leeminh293@users.noreply.github.com>
2022-11-03 14:32:25 +0700

    Lan 1
    Update lan 1

---
+++ Leminhduoc-master/Leminhduoc-master/file.py
@@ -0,0 +1,21 @@
+import time
+
+start_time = time.time()
+
+file=open('pg100.txt','r+')
+
+wordCount={}
+
+for word in file.read().split():
+	if word not in wordCount:
+		wordCount[word]=1
+	else:
+		wordCount[word]+=1
+
+for key in wordCount.keys():
+	print(key,' ',wordCount[key])
+
+file.close()
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
